13e0e77131cd2f57fc7c0d26c4f1a6a3d24c2f46
Clint Stevenson <clintastevenson@gmail.com>
2014-08-14 23:44:43 -0500

    First commit at Rev 4

---
+++ Create_KiCAD_Project_Directory.cmd
@@ -0,0 +1,7 @@
+mkdir %CD%\"Datasheets"
+mkdir %CD%\"Firmware"
+mkdir %CD%\"Hardware\Gerbers"
+mkdir %CD%\"Hardware\KiCAD_Project_Files"
+mkdir %CD%\"Hardware\KiCAD_Project_Archive"
+mkdir %CD%\"Hardware\Gerber_Archive"
+mkdir %CD%\"Rendered_Pics"
