f07e1306903f7d9602156329ae44ad3828913124
Clint Stevenson <clintastevenson@gmail.com>
2014-08-14 23:48:43 -0500

    Removed files inadvertently added in previous commit

--- Create_KiCAD_Project_Directory.cmd
+++
@@ -1,7 +0,0 @@
-mkdir %CD%\"Datasheets"
-mkdir %CD%\"Firmware"
-mkdir %CD%\"Hardware\Gerbers"
-mkdir %CD%\"Hardware\KiCAD_Project_Files"
-mkdir %CD%\"Hardware\KiCAD_Project_Archive"
-mkdir %CD%\"Hardware\Gerber_Archive"
-mkdir %CD%\"Rendered_Pics"
